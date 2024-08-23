5526846004823d5b010f857ecb6eb87cc4eb0bb0
AlbertoTrujillo0221 <57775191+AlbertoTrujillo0221@users.noreply.github.com>
2024-08-22 20:15:54 -0500

    First commit

---
+++ LiteThinkingAugust/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiteThinkingAugust.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => this._mediator;
+
+        protected IMapper Mapper;
+        
+    }
+}
---
+++ LiteThinkingAugust/Controllers/TransactionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiteThinkingAugust.Controllers
+{
+    public class TransactionController : BaseController
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            
+        }
+    }
+}
